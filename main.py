61547808bd821c672e839ab30648028c3d0e77e9
RightOfHeLL <chij.fox@gmail.com>
2026-07-19 02:20:49 +0300

    Add files via upload
    Последовательный запрос данных (день, месяц, год) с обработкой исключений (ValueError) на случай некорректного ввода.
    Определение дня недели с использованием модуля datetime.
    Определение високосного года с помощью встроенного модуля calendar (calendar.isleap).
    Расчет точного возраста с учетом того, прошел ли день рождения в текущем году.
    Вывод даты рождения на "электронное табло". Для этого был создан словарь (Dictionary), где ключами выступают строковые представления цифр от '0' до '9', а значениями — массивы строк, формирующие символы из звездочек. Функция перебирает отформатированную дату (ДД ММ ГГГГ) и выводит соответствующие элементы массивов построчно.
    *Добавлен цикл while True, который не пропустит пользователя дальше, пока он не введет реально существующую дату (учитывая количество дней в конкретном месяце високосного года)
    **get_age_word (теперь работает корректно) проверяет окончания чисел (исключая числа от 11 до 14, которые всегда «лет», а для остальных смотрит на последнюю цифру).

---
+++ main.py
@@ -0,0 +1,152 @@
+import datetime
+import calendar
+
+# День недели
+def get_weekday(day, month, year):
+    days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    date_obj = datetime.date(year, month, day)
+    return days[date_obj.weekday()]
+
+# Високосный год
+def is_leap_year(year):
+    return calendar.isleap(year)
+
+# Расчёт годиков
+def get_age(day, month, year):
+    today = datetime.date.today()
+    birth_date = datetime.date(year, month, day)
+    # Был ли ДР
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
+
+# Склонения слова
+def get_age_word(age):
+    age = int(age)
+    last_two_digits = age % 100
+    last_digit = age % 10
+
+    if 11 <= last_two_digits <= 14:
+        return "лет"
+    
+    # Смотрим ласт цифру
+    if last_digit == 1:
+        return "год"
+    if last_digit in (2, 3, 4):
+        return "года"
+    
+    return "лет"
+
+# ВВод даты звездочками в формате ДД ММ ГГГГ
+def print_date_ascii(day, month, year):
+    date_str = f"{day:02d} {month:02d} {year:04d}"
+    
+    digits = {
+        '0': [" *** ", 
+              "*   *", 
+              "*   *", 
+              "*   *", 
+              " *** "],
+
+        '1': ["  *  ", 
+              " **  ", 
+              "  *  ", 
+              "  *  ", 
+              " *** "],
+
+        '2': [" *** ", 
+              "*   *", 
+              "  *  ", 
+              " *   ", 
+              "*****"],
+
+        '3': [" *** ", 
+              "    *", 
+              "  ** ", 
+              "    *", 
+              " *** "],
+
+        '4': ["   * ", 
+              "  ** ", 
+              " * * ", 
+              "*****", 
+              "   * "],
+
+        '5': ["*****", 
+              "*    ", 
+              "**** ", 
+              "    *", 
+              "**** "],
+
+        '6': [" *** ", 
+              "*    ", 
+              "**** ", 
+              "*   *", 
+              " *** "],
+
+        '7': ["*****", 
+              "    *", 
+              "   * ", 
+              "  *  ", 
+              "  *  "],
+
+        '8': [" *** ", 
+              "*   *", 
+              " *** ", 
+              "*   *", 
+              " *** "],
+
+        '9': [" *** ", 
+              "*   *", 
+              " ****", 
+              "    *", 
+              " *** "],
+              
+        ' ': ["     ", 
+              "     ", 
+              "     ", 
+              "     ", 
+              "     "] #Пробел
+    }
+
+    print("\nДата рождения на электронном табло:")
+    for i in range(5):
+        line = ""
+        for char in date_str:
+            line += digits[char][i] + "  "
+        print(line)
+
+def main():
+    print("Программа анализа даты рождения")
+    
+    while True:
+        try:
+            day = int(input("Введите день рождения: "))
+            month = int(input("Введите месяц рождения: "))
+            year = int(input("Введите год рождения: "))
+            
+            current_year = datetime.date.today().year
+            if year < 1900 or year > current_year:
+                print("Пожалуйста, введите реальный год рождения.\n")
+                continue
+            
+            datetime.date(year, month, day)
+            break 
+            
+        except ValueError:
+            print("Ошибка! Введена несуществующая дата. Пожалуйста, попробуйте снова.\n")
+
+    print(f"\nДень недели: {get_weekday(day, month, year)}")
+    
+    if is_leap_year(year):
+        print(f"Год {year} был високосным.")
+    else:
+        print(f"Год {year} не был високосным.")
+        
+    age = get_age(day, month, year)
+    age_word = get_age_word(age)
+    print(f"Ваш текущий возраст: {age} {age_word}.")
+    
+    print_date_ascii(day, month, year)
+
+if __name__ == "__main__":
+    main()
